66d0a0c201ce646068c46a8d9b49f2467c916306
MCT <tagabalon@gmail.com>
2021-06-22 10:37:33 +0800

    + added player avatar
    - added sprite for player avatar
    - created animator controller for animation
    - added code for simple movement and animation using WASD

---
+++ LSWPrototype/Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LWSPrototype {
+
+	public class PlayerCharacter : MonoBehaviour {
+		private Animator m_Animator;
+
+		public float m_MoveSpeed = 2.0f;
+
+		private void Awake() {
+			m_Animator = GetComponent<Animator>();
+		}
+
+		// Start is called before the first frame update
+		void Start() {
+
+		}
+
+		// Update is called once per frame
+		void Update() {
+			float vertical = Input.GetAxis("Vertical");
+			float horizontal = Input.GetAxis("Horizontal");
+
+			m_Animator.SetFloat("Vertical", vertical);
+			m_Animator.SetFloat("Horizontal", horizontal);
+
+			Vector2 motion = new Vector2(horizontal, vertical);
+			transform.Translate(motion * Time.deltaTime * m_MoveSpeed);
+		}
+	}
+}
